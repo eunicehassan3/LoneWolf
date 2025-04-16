9dad9d055a85578e594854547d8bb927de55f20b
eunicehassan3 <euniceh.info@gmail.com>
2025-04-16 05:06:36 -0500

    updated food and health bar to change colors

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -6,18 +6,39 @@ public class FoodScript : MonoBehaviour
 {
      public GameObject wolf;
     private Slider foodSlider;
+    public Image foodFill; 
+    private float wolfFood;
+    private float maxFood;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         foodSlider = GetComponent<Slider>();
-        // float wolfFood = wolf.GetComponent<PlayerBehavior>().FoodTimer();
+        maxFood = wolfFood = wolf.GetComponent<PlayerBehavior>().maxFood;
+        // foodFill = GetComponentInChildren<Image>();
+        foodFill.color = new Color(0.0f, 0.69f, 0.016f); ; //green
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float wolfFood = wolf.GetComponent<PlayerBehavior>().FoodTimer();
+        wolfFood = wolf.GetComponent<PlayerBehavior>().FoodTimer();
         foodSlider.value = wolfFood;
+        UpdateFoodUI();
+    }
+
+    void UpdateFoodUI()
+    {
+        float foodPercent = wolfFood / maxFood;
+        // foodSlider.value = foodPercent;
+
+        if (foodPercent < 0.2f){
+            foodFill.color =  new Color(0.69f, 0.0f, 0.0f); //red
+        } else if (foodPercent < 0.5f){
+            foodFill.color = new Color(0.68f, 0.40f, 0.02f); // orange
+        } else{
+            foodFill.color = new Color(0.0f, 0.69f, 0.016f); ; //green
+        }
     }
 }
--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -5,18 +5,39 @@ public class HealthScript : MonoBehaviour
 {
     public GameObject wolf;
     private Slider healthSlider;
+    private float wolfHealth;
+    public Image HealthFill;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         healthSlider = GetComponent<Slider>();
+        // HealthFill = GetComponentInChildren<Image>();
+        
+        HealthFill.color = new Color(0.0f, 0.69f, 0.016f); //green
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        float wolfHealth = wolf.GetComponent<PlayerBehavior>().health;
+        wolfHealth = wolf.GetComponent<PlayerBehavior>().health;
         healthSlider.value = wolfHealth;
+        UpdateHealthUI();
+    }
+
+    void UpdateHealthUI()
+    {
+
+        // float foodPercent = wolfFood / maxFood;
+        // healthSlider.value = wolfHealth;
+
+        if (wolfHealth  == 1){
+            HealthFill.color =  new Color(0.69f, 0.0f, 0.0f); //red
+        } else if (wolfHealth  == 2){
+            HealthFill.color = new Color(0.68f, 0.40f, 0.02f); // orange
+        } else{
+            HealthFill.color = new Color(0.0f, 0.69f, 0.016f); ; //green
+        }
     }
 }
