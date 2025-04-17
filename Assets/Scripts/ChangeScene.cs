8ac762feae3d1be3e1484964134a818cf4635fad
eunicehassan3 <euniceh.info@gmail.com>
2025-04-16 21:48:36 -0500

    added main menu and game over scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void GoToMainGame(){
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void GoToMenu(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void GoToWin(){
+        SceneManager.LoadScene("Win");
+    }
+
+    public void GoToGameOver(){
+        SceneManager.LoadScene("GameOver");
+    }
+}
